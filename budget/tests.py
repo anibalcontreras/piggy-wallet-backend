3da3d4fdaa45d3b8201be5cbcdbb3fc1fae8fb78
mjsifri <mjesus.sifri@uc.cl>
2024-06-10 12:29:48 -0400

    fix: black linter

--- budget/tests.py
+++ budget/tests.py
@@ -6,6 +6,7 @@ from authentication.services.cognito_service import CognitoService
 from budget.views import BudgetViewSet
 from rest_framework.response import Response
 
+
 class BudgetViewSetTests(TestCase):
     def setUp(self):
         self.client = APIClient()
@@ -18,15 +19,14 @@ class BudgetViewSetTests(TestCase):
             "AuthenticationResult": {"AccessToken": "mock_access_token", "IdToken": "mock_id_token"}
         }
         mock_create.return_value = Response(status=status.HTTP_201_CREATED, data={"amount": 100})
-        
+
         request = self.client.post("/budget/", {"amount": 100})
         request.headers["Authorization"] = "Bearer mock_access_token"
-        
+
         response = self.client.post("/budget/", {"amount": 100})
-        
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data["amount"], 100)
-
 
     @patch.object(BudgetViewSet, "list")
     @patch.object(CognitoService, "login_user")
@@ -35,12 +35,12 @@ class BudgetViewSetTests(TestCase):
             "AuthenticationResult": {"AccessToken": "mock_access_token", "IdToken": "mock_id_token"}
         }
         mock_list.return_value = Response(status=status.HTTP_200_OK, data={"amount": 100})
-        
+
         request = self.client.get("/budget/")
         request.headers["Authorization"] = "Bearer mock_access_token"
-        
+
         response = self.client.get("/budget/")
-        
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["amount"], 100)
 
@@ -51,12 +51,12 @@ class BudgetViewSetTests(TestCase):
             "AuthenticationResult": {"AccessToken": "mock_access_token", "IdToken": "mock_id_token"}
         }
         mock_destroy.return_value = Response(status=status.HTTP_204_NO_CONTENT)
-        
+
         request = self.client.delete("/budget/")
         request.headers["Authorization"] = "Bearer mock_access_token"
-        
+
         response = self.client.delete("/budget/")
-        
+
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     @patch.object(BudgetViewSet, "partial_update")
@@ -66,11 +66,11 @@ class BudgetViewSetTests(TestCase):
             "AuthenticationResult": {"AccessToken": "mock_access_token", "IdToken": "mock_id_token"}
         }
         mock_partial_update.return_value = Response(status=status.HTTP_200_OK, data={"amount": 200})
-        
+
         request = self.client.put("/budget/", {"amount": 200})
         request.headers["Authorization"] = "Bearer mock_access_token"
-        
+
         response = self.client.put("/budget/", {"amount": 200})
-        
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["amount"], 200)
