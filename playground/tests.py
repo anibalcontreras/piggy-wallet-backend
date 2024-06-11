e4b34312829cbdf29ad2fcf3d62256262869e81c
ANIBAL CONTRERAS <anibal.contreras@uc.cl>
2024-06-11 15:51:00 -0400

    Fix - Test hello world

--- playground/tests.py
+++ playground/tests.py
@@ -6,4 +6,4 @@ class HelloWorldTest(TestCase):
     def test_hello_world(self):
         response = self.client.get(reverse("hello-world"))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "¡Hello World")
+        self.assertContains(response, "Hello World")
