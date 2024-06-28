ac164984a0f14066605440c1bd12ff321a4c0616
Vicente Cruz <vcrb@uc.cl>
2024-06-28 10:27:36 -0400

    bug - corsheaders ModuleNotFound

--- piggywallet/settings/base.py
+++ piggywallet/settings/base.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "playground",
     "authentication",
+    "corsheaders",
     "budget",
     "debt",
     "user_expense_type",
@@ -61,6 +62,8 @@ REST_FRAMEWORK = {
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -69,6 +72,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = "piggywallet.urls"
 
