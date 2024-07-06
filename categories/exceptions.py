1ee5efbb4e45f74c2530c32f772fbf30b9231b2f
ANIBAL CONTRERAS <anibal.contreras@uc.cl>
2024-07-06 14:21:50 -0400

    Fix - Style

--- categories/exceptions.py
+++ categories/exceptions.py
@@ -3,7 +3,7 @@ class InvalidCategoryError(Exception):
         self.category = category
         self.message = (
             "El texto proporcionado no proporciona información suficiente para "
-            + "clasificar el gasto en una categoría. Por favor, se un poco mas descriptivo."
+            "clasificar el gasto en una categoría. Por favor, se un poco mas descriptivo."
         )
         self.code = 400
         super().__init__(self.message)
