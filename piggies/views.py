d5c2a458c6a2cdc921f6d61432aca5ba7eb0cbb1
DaHunterTime <ignacio.huntley@uc.cl>
2024-07-06 15:12:32 -0400

    refactor: deleted legacy code

--- piggies/views.py
+++ piggies/views.py
@@ -11,25 +11,6 @@ from .serializers import PiggiesSerializer
 
 
 class PiggiesViewSet(viewsets.ViewSet):
-    def get_user_id_from_token(self, request):
-        try:
-            authorization_header = request.headers.get("Authorization")
-            if not authorization_header:
-                raise Exception("Authorization header not found")
-
-            token = authorization_header.split()[1]
-            decoded_token = jwt.decode(token, options={"verify_signature": False})
-            username = decoded_token.get("username")
-            if not username:
-                raise Exception("User ID not found in token")
-            return username
-        except jwt.DecodeError:
-            raise Exception("Invalid token")
-        except jwt.ExpiredSignatureError:
-            raise Exception("Expired token")
-        except Exception as e:
-            raise Exception(f"Error decoding token: {e}")
-
     @cognito_authenticated
     def list(self, request):
         try:
